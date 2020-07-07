b99c6c61b0600c164079dab0e71ddf426879d577
SohelRaja <rohan.nsec@gmail.com>
2020-07-07 15:28:39 +0530

    make it better

--- router.js
+++ router.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.send('Server is Up and Running')
-});
+// router.get('/', (req,res) => {
+//     res.send('Server is Up and Running')
+// });
 
 module.exports = router;
